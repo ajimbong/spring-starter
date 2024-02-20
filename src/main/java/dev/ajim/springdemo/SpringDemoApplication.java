c3d9670306cac15ddcb5239477034d3b159e4e0b
ajimbong <ajimbong713@gmail.com>
2024-02-20 19:25:43 +0100

    did some stuff on working with configuratin profiles

--- src/main/java/dev/ajim/springdemo/SpringDemoApplication.java
+++ src/main/java/dev/ajim/springdemo/SpringDemoApplication.java
@@ -4,17 +4,23 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringDemoApplication implements CommandLineRunner {
-	@Value("${demo.message: Default Default info here}")
-	int customProperty;
+	@Value("${spring.profiles.active: default}")
+	String activeProfile;
+
+	@Value("${demo.message: nothing}")
+	String demoMessage;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
+	@Profile("dev")
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("System start success. Message: " + customProperty);
+		System.out.println("Application started on " + activeProfile + " profile");
+		System.out.println("Message: " + demoMessage);
 	}
 }
