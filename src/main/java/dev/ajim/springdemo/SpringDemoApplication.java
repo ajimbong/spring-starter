9c7c2cbaf12b08eb8b60afa024bea87ce4694405
ajimbong <ajimbong713@gmail.com>
2024-02-20 18:35:08 +0100

    completed tasks on spring configuration

--- src/main/java/dev/ajim/springdemo/SpringDemoApplication.java
+++ src/main/java/dev/ajim/springdemo/SpringDemoApplication.java
@@ -1,18 +1,20 @@
 package dev.ajim.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringDemoApplication implements CommandLineRunner {
-
+	@Value("${demo.message: Default Default info here}")
+	int customProperty;
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("I have ran bro");
+		System.out.println("System start success. Message: " + customProperty);
 	}
 }
